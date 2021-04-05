8a601a80917d9b85d7c61dd441c6f5cbb98db116
Cold5nap <and,andr2013@yandex.ru>
2021-04-05 20:34:28 +0300

    Добавьте файлы проекта.

---
+++ stringJenia.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
+	char buff[150]; // буфер промежуточного хранения считываемого из файла текста
+	ifstream fin("cppstudio.txt"); // открыли файл для чтения
+	fin.getline(buff, 150);
+
+	fin.close();
+	system("pause");
+	return 0;
+}
